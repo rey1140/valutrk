c45392a0e50650a192034d75259950d5b8610744
rey1140 <reybalatbat@live.com>
2021-02-12 22:55:52 -0700

    Add files via upload

--- KTLDemo.py
+++ KTLDemo.py
@@ -1,38 +1,74 @@
 import streamlit as st 
 import plotly.express as px 
 import pandas as pd 
-import cufflinks as cf 
+#import cufflinks as cf 
 import seaborn  as sns 
 import numpy as np 
 import plotly.graph_objects as go
 import matplotlib.pyplot as plt 
-from scipy import stats 
+#from scipy import stats 
+#import base64
+#from PIL import Image
+#import json
+#import urllib.request
+#import hashlib
 
-
+html_temp = """
+        <body style="background-color:blue;">
+        <div style="background-color:blue;padding:16px">
+        <h2 style="color:white;text-align:center;">ValuTrk Data Analysis </h2>
+        </div>
+        </body>
+    """
+st.markdown(html_temp, unsafe_allow_html=True)
 
 #Title
-st.title("Keeteelee Studio Data Analysis")
+#st.title("VALUTRK Data Analysis Demo")
 
 #Sidebar
 st.sidebar.subheader("Spend report")
 
 #Setup
 uploaded_file = st.sidebar.file_uploader(
-	label="Upload Files 200mb max size:EtsySoldOrders2020.csv ",
+	label="Upload Files 200mb max size:CSV ",
 	type=['csv','xlsx'])
 
 
 global df 
 if uploaded_file is not None:
+	file_details = {"filename":uploaded_file.name, "filetype":uploaded_file.type,"filesize":uploaded_file.size}
+	st.write(file_details)
+	f_name = file_details["filename"]
 	print(uploaded_file)
 	print("Hello")
 	try:
 		df = pd.read_csv(uploaded_file)
-		#if uploaded_file == "EtsySoldOrders2020.csv":
-		df[['Kyear','Kmonth','Kday']]=df['Sale Date'].str.split('-',expand=True)
-		df['Sale Date'] = pd.to_datetime(df['Sale Date'])
-		df['Day of Week'] = df['Sale Date'].dt.day_name()
-		df = pd.melt(df, id_vars=['Order total','Day of Week','Delivery Country','Kmonth'], value_vars=['Discount Amount','Delivery Discount','Delivery','Sales tax'])
+		st.write(f_name)
+		if  f_name == "EtsySoldOrders2020.csv":
+			df[['Kyear','Kmonth','Kday']]=df['Sale Date'].str.split('-',expand=True)
+			df['Sale Date'] = pd.to_datetime(df['Sale Date'])
+			df['Day of Week'] = df['Sale Date'].dt.day_name()
+			df = pd.melt(df, id_vars=['Order total','Day of Week','Delivery Country','Kmonth'], 
+				value_vars=['Discount Amount','Delivery Discount','Delivery','Sales tax'],var_name = ['Expense'])
+	
+		elif	f_name == "ChitChat Export.csv":
+				df[['Kyear','Kmonth','Kday']]=df['ship_date'].str.split('-',expand=True)
+				df['ship_date'] = pd.to_datetime(df['ship_date'])
+				df['Day of Week'] = df['ship_date'].dt.day_name()
+				df = pd.melt(df, id_vars=['purchase_amount','carrier','weight','postage_type','return_country_code','Day of Week','Kmonth'], 
+					value_vars=['delivery_fee','insurance_fee','postage_fee','federal_tax','provincial_tax'],
+					var_name = 'Logistic_Type',value_name='Shipment Fees')
+	
+		elif	f_name == "EtsyListingsDownload.csv":
+				df[['Kyear','Kmonth','Kday']]=df['Sale Date'].str.split('-',expand=True)
+				df['Sale Date'] = pd.to_datetime(df['Sale Date'])
+				df['Day of Week'] = df['Sale Date'].dt.day_name()
+				df = pd.melt(df, id_vars=['Delivery City','Delivery State','Delivery Country','Order Value','Order total','Order Net','value','Day of Week','Kmonth'], 
+					value_vars=['Order Value','Delivery','Sales Tax','Discount Amount','Delivery Discount'],
+					var_name = 'Calculated Total',value_name='Other Expense')
+
+
+
 	except Exception as e:
 		print(e)
 		df = pd.read_excel(uploaded_file)
@@ -40,15 +76,16 @@ if uploaded_file is not None:
 
 
 
-global numeric_columns
-try:
+
+	global numeric_columns
+	try:
 #	df = pd.melt(df, id_vars=['Order total','Day of Week','Delivery Country','Kmonth'], value_vars=['Discount Amount','Delivery Discount','Delivery','Sales tax'])
-	st.write(df)
+		st.write(df)
 #	numeric_columns = list(df.select_dtypes(['float','int']).columns)
-	numeric_columns = list(df.columns)
-except Exception as e:
-	print(e)
-	st.write("Please upload file to the application.")
+		numeric_columns = list(df.columns)
+	except Exception as e:
+		print(e)
+		st.write("Please upload file to the application.")
 
 #add a select widget to teh side bar
 chart_select = st.sidebar.selectbox(
